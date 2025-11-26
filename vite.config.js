88d1832273f343683a45fce4125aecdc724e07c4
Revati Natu <revatinatu@hotmail.com>
2025-11-27 00:36:44 +0530

    HOPEFULLY

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/runtime-archives/',  // ADD THIS LINE
+  base: '/Runtime-Archives/',  
   server: {
     port: 3000,
     open: true
