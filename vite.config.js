dd8a880995128ffcffae69aefca97b94d694a887
Revati Natu <revatinatu@hotmail.com>
2025-11-30 21:45:30 +0530

    oops

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Runtime-Archives/',
+  base: '/runtime-archives/',
   build: {
     outDir: 'dist',
   },
